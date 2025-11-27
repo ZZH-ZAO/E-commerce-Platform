e0a25eeded2fc6e1700b13a996fbd907edb1d0d9
ZZH-ZAO <1416686359@qq.com>
2025-11-27 22:55:02 +0800

    axios基础配置

--- vue3-rabbit/src/main.js
+++ vue3-rabbit/src/main.js
@@ -1,7 +1,19 @@
-import "./assets/main.css";
 //new vue 创建一个应用实例对象
 import { createApp } from "vue";
 import App from "./App.vue";
+import { createPinia } from "pinia";
+// import router from "./router";
+import "./assets/main.css";
+
+//测试接口函数
+import { getCategory } from "@/api/testAPI.js";
+getCategory().then((res) => {
+  console.log("测试接口函数结果：", res);
+});
+const app = createApp(App);
+
+app.use(createPinia());
+app.use(router);
 //以APP为参数创建一个应用实例对象
 //挂载到id为app的节点上
-createApp(App).mount("#app");
+app.mount("#app");
