34059d819504bf548222a27dd20dfc1bf4cacadc
ZZH-ZAO <1416686359@qq.com>
2025-11-27 22:22:35 +0800

    定制了elementPlus主题色

---
+++ vue3-rabbit/src/main.js
@@ -0,0 +1,7 @@
+import "./assets/main.css";
+//new vue 创建一个应用实例对象
+import { createApp } from "vue";
+import App from "./App.vue";
+//以APP为参数创建一个应用实例对象
+//挂载到id为app的节点上
+createApp(App).mount("#app");
