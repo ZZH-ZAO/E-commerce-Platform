06b4a07b0980c7091601958780c802d14f209953
ZZH-ZAO <1416686359@qq.com>
2025-11-29 21:31:57 +0800

    layout静态模板结构搭建

--- vue3-rabbit/src/views/Layout/index.vue
+++ vue3-rabbit/src/views/Layout/index.vue
@@ -1,8 +1,12 @@
-<!-- src/views/Layout/index.vue -->
+<script setup>
+import LayoutNav from './components/LayoutNav.vue'
+import LayoutHeader from './components/LayoutHeader.vue'
+import LayoutFooter from './components/LayoutFooter.vue'
+</script>
+
 <template>
-  <div>
-    <div>我是首页（Layout 父路由）</div>
-    <!-- 二级路由出口：这里会显示 Home / Category -->
-    <RouterView />
-  </div>
+  <LayoutNav />
+  <LayoutHeader />
+  <RouterView />
+  <LayoutFooter />
 </template>
